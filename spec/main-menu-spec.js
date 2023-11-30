34e418f1b4f073980b6dc8fa8f18c9afd4fdd1d5
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-30 08:32:43 +0530

    Update main-menu-spec.js

--- spec/main-menu-spec.js
+++ spec/main-menu-spec.js
@@ -6,7 +6,6 @@ const expected = {
   attachment: {
     type: 'template',
     payload: {
-      
       template_type:'generic',
       elements: [{
         title: 'NASA\'s Astronomy Picture of the Day',
