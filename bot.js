b871031bc4bf89d349689a97985c2bcdfd227308
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-22 11:37:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,5 @@ const api = botBuilder(botFlow, {
 
 
 api.addPostDeployConfig('nasaApiKey', 'NASA API Key:', 'configure-bot')
-
 api.addPostDeployConfig('apiAiToken', 'Api.ai token:', 'configure-bot')
 module.exports = api
